fa91867ebdf9258daa2a9319e5652aaf482b3d43
wamgyikai19 <wangyikai19@gmail.com>
2025-01-08 11:33:50 +0800

    add CollectStore

--- Rocket6w6wH/Models/CollectStore.cs
+++ Rocket6w6wH/Models/CollectStore.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace Rocket6w6wH.Models
 {
 	public class CollectStore
 	{
-	}
+        [Key]
+        [Display(Name = "編號")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "店家ID")]
+        public int StoreId { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey("StoreId")]
+        public virtual Stores Stores { get; set; }
+
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "按讚人ID")]
+        public int MemberId { get; set; }
+        [JsonIgnore]
+        [ForeignKey("MemberId")]
+        public virtual Member Member { get; set; }
+        public DateTime? CreateTime { get; set; }
+    }
 }
