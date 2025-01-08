276d00a2930d8cb7b4ecf2673b2a0eae3c4e1709
wamgyikai19 <wangyikai19@gmail.com>
2025-01-08 11:04:24 +0800

    復原

---
+++ Rocket6w6wH/Models/CommentLike.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Rocket6w6wH.Models
+{
+	public class CommentLike
+	{
+        [Key]
+        [Display(Name = "編號")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "回覆編號")]
+        public int CommentId { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey("CommentId")]
+        public virtual StoreComments StoreComments { get; set; }
+
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "按讚人ID")]
+        public int LikeUserId { get; set; }
+        public virtual Member Member { get; set; }
+        public DateTime? CreateTime { get; set; }
+    }
+}
